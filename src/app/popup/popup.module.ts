2a715a1e2be2c53ee83c9f4c6187433426eab470
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-06-22 14:29:05 +0200

    build(ng): generate popup module

---
+++ src/app/popup/popup.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PopupRoutingModule } from './popup-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PopupRoutingModule
+  ]
+})
+export class PopupModule { }
