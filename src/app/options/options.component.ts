10ba9a26af0e4b8c89a7f8168dd1dd6ff3e0370a
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-06-23 13:45:43 +0200

    feat(background): create base

--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-options',
   templateUrl: './options.component.html',
-  styleUrls: ['./options.component.scss']
+  styleUrls: ['./options.component.scss'],
 })
 export class OptionsComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    console.log('options');
   }
-
 }
--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-popup',
   templateUrl: './popup.component.html',
-  styleUrls: ['./popup.component.scss']
+  styleUrls: ['./popup.component.scss'],
 })
 export class PopupComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    console.log('popup');
   }
-
 }
