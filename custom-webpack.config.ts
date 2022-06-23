e0e9587814790e73399c350a4ee48e28200c1c97
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-06-23 13:58:50 +0200

    feat(content_scripts): create base

--- custom-webpack.config.ts
+++ custom-webpack.config.ts
@@ -1,5 +1,8 @@
 import type { Configuration } from 'webpack';
 
 module.exports = {
-  entry: { background: 'src/background.ts' },
+  entry: {
+    background: 'src/background.ts',
+    content_scripts: 'src/content_scripts.ts',
+  },
 } as Configuration;
