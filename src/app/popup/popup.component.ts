d0590207d9ce17ee26157143d2e14aa544a2ac60
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-06-27 15:17:38 +0200

    refactor(popup.component): adjust colorize()

--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -21,14 +21,14 @@ export class PopupComponent implements OnInit {
   }
 
   colorize(): void {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      chrome.scripting.executeScript({
-        target: { tabId: tabs[0].id as number },
-        func: setPageBackgroundColor,
-        args: [this.color],
+    chrome.tabs.query({ active: true, currentWindow: true }, ([currentTab]) => {
+      chrome.storage.sync.set({ color: this.color }).then(() => {
+        chrome.scripting.executeScript({
+          target: { tabId: currentTab.id as number },
+          func: setPageBackgroundColor,
+          args: [this.color],
+        });
       });
-
-      chrome.storage.sync.set({ color: this.color });
     });
   }
 
