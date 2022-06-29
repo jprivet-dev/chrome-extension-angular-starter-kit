b469814041abbad69dd8964917da97509f8fa740
jprivet-dev <jean.philippe.rivet@gmail.com>
2022-06-29 13:41:54 +0200

    feat(options): add list of applied colors

--- src/app/options/options.component.ts
+++ src/app/options/options.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ColorData } from '../shared/color.model';
+import { STORAGE_COLORS } from '../shared/storage.constant';
 
 @Component({
   selector: 'app-options',
@@ -6,13 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./options.component.scss'],
 })
 export class OptionsComponent implements OnInit {
-  color: string = '#ffffff';
+  colorPicker: string = '#ffffff';
+  colors: ColorData[] = [];
+
+  constructor(private ref: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     console.info('options executed!');
+    this.refreshColorsFromStorage();
+
+    chrome.storage.onChanged.addListener((changes, namespace) => {
+      if (STORAGE_COLORS in changes) {
+        this.refreshColorsFromStorage(true);
+      }
+    });
   }
 
   select(color: string): void {
-    this.color = color;
+    this.colorPicker = color;
+  }
+
+  private refreshColorsFromStorage(detectChanges: boolean = false) {
+    chrome.storage.sync.get(STORAGE_COLORS, ({ colors }) => {
+      this.colors = colors;
+      if (detectChanges) {
+        this.ref.detectChanges();
+      }
+    });
   }
 }
